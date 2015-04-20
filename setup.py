5ae780e18d2d35587b6fed1785c8d01bd5b8631f
austburn <austburn@gmail.com>
2015-04-20 10:32:41 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name="rax-python",
+  description="Rackspace Python class witht Glyph Lefkowitz",
+  packages=[
+    "rax"
+  ],
+  install_requires=[
+    "requests[security]"
+  ],
+  license="MIT",
+  version="0.0.1"
+)
