0d63d71c18164889c43aa29b0642e077867ed4e6
austburn <austburn@gmail.com>
2015-04-20 11:56:47 -0400

    Decorators pt. 6, add args/kwargs

--- decorators.py
+++ decorators.py
@@ -4,26 +4,28 @@ from functools import wraps
 
 def logged(function):
     @wraps(function)
-    def a_logged_thing():
-        print("Starting", function)
+    def a_logged_thing(*args, **kwargs):
+        print("Starting", function,
+              args, kwargs)
         try:
-            function()
+            function(*args, **kwargs)
         finally:
-            print("Ending", function)
+            print("Ending", function,
+                  args, kwargs)
     return a_logged_thing
 
 
 @logged
 @logged
-def hello():
-    print("Hello!")
+def hello(world):
+    print("Hello!", world)
 
 
-hello()
+hello("Austin")
 """
-Starting <function hello at 0x7fda3b637a28>
-Starting <function hello at 0x7fda3b6379b0>
-Hello!
-Ending <function hello at 0x7fda3b6379b0>
-Ending <function hello at 0x7fda3b637a28>
+Starting <function hello at 0x7f28c0752aa0> ('Austin',) {}
+Starting <function hello at 0x7f28c0752a28> ('Austin',) {}
+Hello! Austin
+Ending <function hello at 0x7f28c0752a28> ('Austin',) {}
+Ending <function hello at 0x7f28c0752aa0> ('Austin',) {}
 """
