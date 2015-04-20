6fb36edc809129a28bde9d9708e11fb511f0f7e3
austburn <austburn@gmail.com>
2015-04-20 13:42:48 -0400

    finishing up generators.

--- generators.py
+++ generators.py
@@ -1,18 +1,16 @@
 from __future__ import print_function
 
 
-def one_two_three_four(base):
-    n = 0
-    while n < 4:
-        yield base + n
-        n = n + 1
+def wait_for(x):
+    print("waiting for", x)
+    yield
+    print(x[0])
 
 
-for value in one_two_three_four(7):
-    print("iterated", value)
-"""
-iterated 7
-iterated 8
-iterated 9
-iterated 10
-"""
+nothing_yet = []
+
+
+g = wait_for(nothing_yet)
+next(g)
+nothing_yet.append(3)
+next(g)
