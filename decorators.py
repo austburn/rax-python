8a2fde84f454b5666a863789fe81e4777e142df3
austburn <austburn@gmail.com>
2015-04-20 11:45:24 -0400

    Decorators pt 2

--- decorators.py
+++ decorators.py
@@ -2,19 +2,22 @@ from __future__ import print_function
 
 
 def logged(function):
-    print("Starting", function)
-    try:
-        function()
-    finally:
-        print("Ending", function)
+    def a_logged_thing():
+        print("Starting", function)
+        try:
+            function()
+        finally:
+            print("Ending", function)
+    return a_logged_thing
 
 
 def hello():
     print("Hello!")
 
-logged(hello)
+logged_hello = logged(hello)
+logged_hello()
 """
-Starting <function hello at 0x7f4800ba47d0>
+Starting <function hello at 0x7fbc034b47d0>
 Hello!
-Ending <function hello at 0x7f4800ba47d0>
+Ending <function hello at 0x7fbc034b47d0>
 """
