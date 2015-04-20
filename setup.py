06d21bfaffdd694b94c8d1b0d343de2474ed349f
austburn <austburn@gmail.com>
2015-04-20 14:01:11 -0400

    entry points

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
 setup(
-  name="rax-python",
-  description="Rackspace Python class witht Glyph Lefkowitz",
-  packages=[
-    "rax"
-  ],
-  install_requires=[
-    "requests[security]"
-  ],
-  license="MIT",
-  version="0.0.1"
+    name="rax-python",
+    description="Rackspace Python class witht Glyph Lefkowitz",
+    packages=[
+        "rax"
+    ],
+    install_requires=[
+        "requests[security]"
+    ],
+    license="MIT",
+    version="0.0.1",
+    entry_points = {
+        "console_scripts": [
+            "say-hello=entry:some_func"
+        ]
+    }
 )
