69443e50f4a85f0f610d771049eba3b57b0e168d
austburn <austburn@gmail.com>
2015-04-20 12:09:19 -0400

    gen pt 2

--- generators.py
+++ generators.py
@@ -1,18 +1,18 @@
 from __future__ import print_function
 
 
-def one_two_three_four():
-    yield 1
-    yield 2
-    yield 3
-    yield 4
+def one_two_three_four(base):
+    yield base + 1
+    yield base + 2
+    yield base + 3
+    yield base + 4
 
 
-for value in one_two_three_four():
+for value in one_two_three_four(7):
     print("iterated", value)
 """
-iterated 1
-iterated 2
-iterated 3
-iterated 4
+iterated 8
+iterated 9
+iterated 10
+iterated 11
 """
