65906405f49797117dbd653c36cb0c76b6d0aec6
austburn <austburn@gmail.com>
2015-04-20 12:16:05 -0400

    yield with exceptions

--- ctxman.py
+++ ctxman.py
@@ -4,9 +4,16 @@ from contextlib import contextmanager
 @contextmanager
 def example():
     print("starting!")
-    yield 3
-    print("finishing up...")
+    try:
+        yield 3
+    except ZeroDivisionError:
+        print("caught exception")
+    finally:
+        print("finishing up...")
 
 
 with example() as something:
     print(something)
+    1/0
+
+print("guarded?!")
